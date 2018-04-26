1a4980a3e58b6b4f36683afcc337df2cb48e44ff
Francesco Sacchi <sacchi@intre.it>
2018-04-26 21:44:44 +0200

    Small refactor to get neighborhood

--- src/main/kotlin/it/intre/GameOfLife.kt
+++ src/main/kotlin/it/intre/GameOfLife.kt
@@ -6,18 +6,19 @@ fun evolve(world: List<Char>): List<Char> {
 
 fun evolveCell(idx: Int, cell: Char, world: List<Char>): Char = if (countAliveNeighbours(idx, world) == 2 ) '*' else '.'
 
-fun getNeighbours(idx: Int, world: List<Char>): List<Char> = listOf(before(idx, world), after(idx, world))
-fun isAlive(cell: Char) = cell == '*'
 fun countAliveNeighbours(idx: Int, world: List<Char>) =
         getNeighbours(idx, world)
                 .filter(::isAlive)
                 .count()
 
+fun getNeighbours(idx: Int, world: List<Char>): List<Char> = neighborhood.map { it.invoke(idx, world)}
+val neighborhood = listOf(::before, ::after)
 fun before(idx: Int, world: List<Char>) = world.getOrElse(idx - 1, ::getNone)
 fun after(idx: Int, world: List<Char>) = world.getOrElse(idx + 1, ::getNone)
+
 fun getNone(idx: Int) = '.'
 
-
+fun isAlive(cell: Char) = cell == '*'
 
 fun <T, R> kotlin.collections.List<T>.myMap(transform: (kotlin.Int, T, List<T>) -> R): kotlin.collections.List<R> {
     return this.mapIndexed { idx: Int, e: T -> transform.invoke(idx, e, this) }
