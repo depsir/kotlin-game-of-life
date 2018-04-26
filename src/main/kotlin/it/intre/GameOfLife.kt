37d4e80d352fe43cacf5d35207e5539d3c828d28
Francesco Sacchi <sacchi@intre.it>
2018-04-26 22:45:51 +0200

    Bidimensional world with something functional

--- src/main/kotlin/it/intre/GameOfLife.kt
+++ src/main/kotlin/it/intre/GameOfLife.kt
@@ -1,7 +1,20 @@
 package it.intre
 
-fun evolve(world: List<Char>): List<Char> {
-    return world.myMap(::evolveCell)
+import org.funktionale.currying.*
+import org.funktionale.composition.*
+
+val map: ((Int, Char, List<Char>) -> Char) -> (List<Char>) -> List<Char> = { transform: (kotlin.Int, Char, List<Char>) -> Char, collection: kotlin.collections.List<Char> ->
+    collection.mapIndexed { idx: Int, e: Char -> transform.invoke(idx, e, collection) }
+}.curried()
+val flatten: (List<List<Char>>) -> List<Char> = { world: List<List<Char>> -> world.flatten()}
+val windowed = { size: Int, step: Int, flatWorld:List<Char> ->
+    flatWorld.windowed(size, step)
+}.curried()
+
+fun evolve(world: List<List<Char>>): List<List<Char>> {
+    val worldWidth = world.first().size
+    val evolveWorld = flatten andThen map(::evolveCell) andThen windowed(worldWidth)(worldWidth)
+    return if (worldWidth == 0) world else evolveWorld(world)
 }
 
 fun evolveCell(idx: Int, cell: Char, world: List<Char>): Char = if (countAliveNeighbours(idx, world) == 2 ) '*' else '.'
@@ -19,8 +32,4 @@ fun after(idx: Int, world: List<Char>) = world.getOrElse(idx + 1, ::getNone)
 fun getNone(idx: Int) = '.'
 
 fun isAlive(cell: Char) = cell == '*'
-
-fun <T, R> kotlin.collections.List<T>.myMap(transform: (kotlin.Int, T, List<T>) -> R): kotlin.collections.List<R> {
-    return this.mapIndexed { idx: Int, e: T -> transform.invoke(idx, e, this) }
-}
 
--- src/test/kotlin/it/intre/GameOfLifeKtTest.kt
+++ src/test/kotlin/it/intre/GameOfLifeKtTest.kt
@@ -6,19 +6,25 @@ import org.junit.Test
 class GameOfLifeKtTest{
     @Test
     fun `an empty world evolves in an empty world`() {
-        assertEquals(listOf<Char>(), evolve(listOf()))
+        assertEquals(listOf<List<Char>>(listOf()), evolve(listOf(listOf())))
     }
 
     @Test
     fun `a world with a single cell evolves in a world with a dead cell`() {
-        assertEquals(listOf('.'), evolve(listOf('.')))
-        assertEquals(listOf('.'), evolve(listOf('*')))
+        assertEquals(listOf(listOf('.')), evolve(listOf(listOf('.'))))
+        assertEquals(listOf(listOf('.')), evolve(listOf(listOf('*'))))
     }
 
     @Test
     fun `a cell with two neighbours stays alive`() {
-        assertEquals(listOf('.','*','.'), evolve(listOf('*','*','*')))
+        assertEquals(listOf(listOf('.','*','.')), evolve(listOf(listOf('*','*','*'))))
     }
+
+    @Test
+    fun `a 2d world!`() {
+        assertEquals(listOf(listOf('.','*','.')), evolve(listOf(listOf('*','*','*'))))
+    }
+
 }
 
 
