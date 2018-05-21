41213eb7040decfd81f5a15dd565a80b3b76196d
Francesco Sacchi <sacchi@intre.it>
2018-05-21 22:27:53 +0200

    Better function naming

--- src/main/kotlin/it/intre/GameOfLife.kt
+++ src/main/kotlin/it/intre/GameOfLife.kt
@@ -5,15 +5,17 @@ import arrow.syntax.function.curried
 
 typealias World = List<List<Char>>
 data class Position(val row:Int, val column:Int)
-val getPosition = {world: World, position: Position -> world.getOrNull(position.row)?.getOrNull(position.column) ?: '.'}.curried()
+val getCellValue = { world: World, position: Position -> world.getOrNull(position.row)?.getOrNull(position.column) ?: '.'}.curried()
 val translate = { first:Position, other: Position -> Position(first.row + other.row, first.column + other.column)}.curried()
 
 fun evolve(world: World): World {
     return world.mapIndexed { row, list -> list.mapIndexed { col, cell ->
-        (countAliveNeighbours(world) andThen evolveCell(cell))(Position(row,col))} }
+        val evolveCell = countAliveNeighbours(world) andThen computeNextState(cell)
+        evolveCell(Position(row,col))
+    }}
 }
 
-val evolveCell = {cell:Char, numberOfAliveNeighbours:Int ->
+val computeNextState = { cell:Char, numberOfAliveNeighbours:Int ->
      when (numberOfAliveNeighbours) {
         2 -> cell
         3 -> '*'
@@ -24,7 +26,7 @@ val evolveCell = {cell:Char, numberOfAliveNeighbours:Int ->
 val countAliveNeighbours = {world: World, position: Position ->
     val neighbourPositions = listOf(Position(-1,-1), Position(-1,0), Position(-1,1), Position(0,-1),Position(0,1),Position(1,-1),Position(1,0),Position(1,1))
     neighbourPositions
-            .map( translate(position) andThen getPosition(world))
+            .map( translate(position) andThen getCellValue(world))
             .filter(::isAlive)
             .count()
 }.curried()
