833a372ca854a35cb668067b84d3011e25dce41c
mar <marcato@intre.it>
2018-05-26 19:42:56 +0200

    beer-refactor

--- src/main/kotlin/it/intre/GameOfLife.kt
+++ src/main/kotlin/it/intre/GameOfLife.kt
@@ -3,30 +3,69 @@ package it.intre
 import arrow.syntax.function.andThen
 import arrow.syntax.function.curried
 
-typealias World = List<List<Char>>
+data class World<out A>(val ll: List<List<A>>)
+{
+    fun getOrNull(row:Int, column: Int) {
+        ll.getOrNull(row)
+                ?.getOrNull(column)
+    }
 
+    fun <B>map(f : (A) -> B):World<B>   {
+        return World(ll.map { list ->
+            list.map { cell ->
+                f(cell)
+            }
+        })
+    }
+
+    fun <B>mapIndexed(f : (A, Pair<Int, Int>) -> B):World<B>   {
+        return World(ll.mapIndexed { row, list ->
+            list.mapIndexed { col, cell ->
+                f(cell, row to col)
+            }
+        })
+    }
+
+data class Cell(val state: Char, val aliveNs: Int)
 data class Position(val row: Int, val column: Int)
+
 
 val DEAD_CELL = '.'
 val ALIVE_CELL = '*'
-val getCellValue = { world: World, position: Position ->
-    world.getOrNull(position.row)
-            ?.getOrNull(position.column)
+val getCellValue = { world: World<Char>, position: Position ->
+    world.getOrNull(position.row, position.column)
             ?: DEAD_CELL
 }
         .curried()
-val translate = { first: Position, other: Position -> Position(first.row + other.row, first.column + other.column) }.curried()
+val translate = { first: Position,
+                  other: Position ->
+    Position(first.row + other.row, first.column + other.column)
+}.curried()
 
-fun evolve(world: World): World {
-    return world.mapIndexed { row, list ->
-        list.mapIndexed { col, cell ->
-            val evolveCell = countAliveNeighbours(world) andThen computeNextState(cell)
-            evolveCell(Position(row, col))
+fun evolve(world: World<Char>): World<Char> {
+    return computeNextStates(toCell(world, addPosition(world)))
+}
+
+fun computeNextStates(w: World<Cell>): World<Char> {
+    return w.map { cell ->
+            computeNextState(cell)
         }
+}
+
+fun addPosition(world: World<Char>): World<Pair<Char, Position>> {
+    return world.mapIndexed { cell, pair ->
+            cell to Position(pair.first, pair.second)
     }
 }
 
-val countAliveNeighbours = { world: World, position: Position ->
+
+fun toCell(world: World<Char>, ll: World<Pair<Char, Position>>): World<Cell> {
+    return ll.map { pair ->
+            Cell(pair.first, countAliveNeighbours(world)(pair.second))
+    }
+}
+
+val countAliveNeighbours = { world: World<Char>, position: Position ->
     (-1..1).flatMap { x ->
         (-1..1).map { y ->
             x to y
@@ -39,13 +78,12 @@ val countAliveNeighbours = { world: World, position: Position ->
             .count()
 }.curried()
 
-val computeNextState = { cell: Char, numberOfAliveNeighbours: Int ->
-    when (numberOfAliveNeighbours) {
-        2 -> cell
+val computeNextState = { cell: Cell ->
+    when (cell.aliveNs) {
+        2 -> cell.state
         3 -> ALIVE_CELL
-
         else -> DEAD_CELL
     }
-}.curried()
+}
 
 fun isAlive(cell: Char) = cell == ALIVE_CELL
